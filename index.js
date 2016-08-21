9b358c111b4492d740cd7398a4a88b901349b67f
Ferrari Lee <shiyung@gmail.com>
2016-08-21 18:57:46 +0800

    support basic webhook behavior

--- index.js
+++ index.js
@@ -3,26 +3,26 @@
 'use strict'
 
 const fs = require('fs')
-const path = require('path')
 const readline = require('readline')
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 })
-const handler = require(path.resolve(__dirname, 'lib/handler.js'))
 
 const SUPPORT_TYPE = [
   'auth',
   'get-client',
   'get-list',
+  'show-list-info',
   'get-list-webhook',
   'create-list-webhook',
   'delete-list-webhook'
 ]
-const CONF_PATH = handler.CONF_PATH()
+const CampaignMonitorHandler = require('./lib/handler.js').CampaignMonitorHandler
+const CONF_PATH = require('./lib/handler.js').GetConf()
 
 checkSetting()
-.then(() => {
+.then((token) => {
   const argv = require('yargs')
   .usage('Usage: $0 -t [type] <options>')
   .alias('t', 'type')
@@ -33,17 +33,39 @@ checkSetting()
   .version(require('./package.json').version)
   .argv
 
+  let campaignMonitor = new CampaignMonitorHandler(token)
+
   switch (argv.type) {
     case 'auth':
-    console.log(argv._, argv._.length)
+      console.log(argv._, argv._.length)
       if (argv._.length < 1) {
         handleOutput(new Error('Missing api token'))
       } else {
-        handler.updateApiToken(argv._[0], handleOutput)
+        campaignMonitor.updateApiToken(argv._[0], handleOutput)
       }
       break
     case 'get-client':
-      handler.getClients(handleOutput)
+      campaignMonitor.getClients(handleOutput)
+      break
+    case 'get-list':
+      // -t get-list [--search keyword] clientID
+      campaignMonitor.getSubscriberLists(argv._[0], argv, handleOutput)
+      break
+    case 'show-list-info':
+      // -t show-list-info listID
+      campaignMonitor.showListInfo(argv._[0], handleOutput)
+      break
+    case 'get-list-webhook':
+      // -t get-list-webhook listID
+      campaignMonitor.getListWebhook(argv._[0], handleOutput)
+      break
+    case 'create-list-webhook':
+      // -t get-list-webhook listID Events(,) Url
+      campaignMonitor.createListWebhook(argv._, handleOutput)
+      break
+    case 'delete-list-webhook':
+      // -t get-list-webhook listID webhookID
+      campaignMonitor.deleteListWebhook(argv._[0], argv._[1], handleOutput)
       break
     default:
       process.exit(1)
@@ -72,7 +94,6 @@ function checkSetting () {
         if (err) return reject(err)
 
         let token = result.toString('utf8')
-        handler.setAuth(token)
         return resolve(token)
       })
     })
@@ -80,7 +101,6 @@ function checkSetting () {
 }
 
 function handleOutput (err, result) {
-  console.log(err, result)
   if (err) {
     console.error(err)
     process.exit(1)
